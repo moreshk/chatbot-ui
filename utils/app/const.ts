f288b90b618969427caeccdf13fdaf674b98b13e
moreshk <moreshk@gmail.com>
2023-06-01 14:27:46 +1000

    default prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,9 +2,9 @@ import Router from "next/router";
 import { supabase } from '@/lib/supabase';
 import { create } from "zustand";
 
-// export const DEFAULT_SYSTEM_PROMPT =
-//   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-//   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+export const DEFAULT_SYSTEM_PROMPT =
+  process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
+  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
