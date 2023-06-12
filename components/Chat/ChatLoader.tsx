6a0712c3a9b831571be2314e438f0681fa24b3db
Pranav <pranavkp.me@outlook.com>
2023-06-12 16:57:33 +0530

    updated the chat loader

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -7,8 +7,10 @@ export const ChatLoader: FC<Props> = () => {
   return (
     <div className="" style={{ overflowWrap: 'anywhere' }}>
       <div className="relative m-auto flex gap-2 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-        <div className="mr-20 rounded-lg bg-blue-500  py-3 px-4 font-light text-white w-full">
-          <span className="animate-pulse cursor-default mt-1">▍</span>
+        <div className="mr-20 rounded-lg bg-gray-200  py-3 px-4 font-light text-black w-full">
+          <span className="animate-pulse cursor-default mt-1  text-black">
+            ▍
+          </span>
         </div>
       </div>
     </div>
