e6b6abdc76dc5e09bc89dfb67dda608930e5ab6f
Pranav <pranavkp.me@outlook.com>
2023-06-14 12:34:12 +0530

    updated the chat-bot-bubble

--- public/chat-bot-bubble.js
+++ public/chat-bot-bubble.js
@@ -1,118 +1,83 @@
-const container = document.createElement('div');
-const iframe = document.createElement('iframe');
-const currentScript = document.currentScript;
-const chatbotId = currentScript.getAttribute('data-chatbot-id');
-iframe.src = `https://leadqualifier.koretex.ai/?chatbotId=${chatbotId}`;
-iframe.style.display = 'none';
-iframe.id = 'my-iframe';
-iframe.style.border = 'none';
-iframe.style.position = 'fixed';
-iframe.style.flexDirection = 'column';
-iframe.style.justifyContent = 'space-between';
-iframe.style.bottom = '8rem';
-iframe.style.right = '1rem';
-iframe.style.width = '425px';
-iframe.style.height = '75vh';
-iframe.style.maxHeight = '824px';
-iframe.style.borderRadius = '0.75rem';
-iframe.style.zIndex = '999999998';
-iframe.style.overflow = 'hidden';
-iframe.style.left = 'unset';
-iframe.style.transition = 'all 0.5s ease-in-out';
-iframe.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+(function () {
+  document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css" rel="stylesheet">',
+  );
 
-const img = document.createElement('img');
-img.src = 'https://dashboard.koretex.ai/bubble.svg';
-img.style.width = '50px';
-img.style.height = '50px';
-img.style.borderRadius = '100%';
-img.style.animation = 'pulsate 1s infinite';
-
-const keyframes = `@keyframes pulsate {
-  0% { transform: scale(1); }
-  50% { transform: scale(1.02); }
-  100% { transform: scale(1); }
-}`;
-
-const style = document.createElement('style');
-style.appendChild(document.createTextNode(keyframes));
-const button = document.createElement('button');
-
-document.head.appendChild(style);
-
-document.body.appendChild(img);
-
-button.appendChild(img);
-button.style.border = 'none';
-button.style.position = 'fixed';
-button.style.flexDirection = 'column';
-button.style.justifyContent = 'space-between';
-button.style.bottom = '2rem';
-button.style.right = '1rem';
-button.style.background = 'transparent';
-button.style.borderRadius = '0.75rem';
-button.style.display = 'flex';
-button.style.zIndex = '999999997';
-button.style.overflow = 'hidden';
-button.style.left = 'unset';
-button.style.cursor = 'pointer';
-button.style.borderRadius = '100%';
-
-const xMark = document.createElement('div');
-xMark.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
-</svg>
-`;
-xMark.style.fontSize = '40px';
-xMark.style.fontWeight = 'bold';
-xMark.style.marginTop = '6px';
-xMark.style.marginRight = '6px';
-xMark.style.position = 'absolute';
-xMark.style.width = '32px';
-xMark.style.height = '32px';
-xMark.style.top = '10px';
-xMark.style.right = '22px';
-xMark.style.zIndex = '999999999';
-xMark.style.color = '#fff';
-xMark.style.display = 'none';
-xMark.addEventListener('click', () => {
-  xMark.style.display = xMark.style.display === 'none' ? 'block' : 'none';
-  iframe.style.display = iframe.style.display === 'none' ? 'block' : 'none';
-});
-
-button.addEventListener('click', () => {
-  xMark.style.display = xMark.style.display === 'none' ? 'block' : 'none';
-  iframe.style.display = iframe.style.display === 'none' ? 'block' : 'none';
-});
-container.appendChild(xMark);
-
-container.appendChild(iframe);
-container.appendChild(button);
-
-function updateIframeWidth() {
-  if (window.innerWidth < 525) {
-    console.log(window.innerWidth);
-    iframe.style.width = '100%';
-    iframe.style.height = '100vh';
-    iframe.style.left = '0rem';
-    iframe.style.maxHeight = '100vh';
-    iframe.style.bottom = '0rem';
-    xMark.style.top = '-2px';
-    xMark.style.right = '6px';
-  } else {
-    iframe.style.width = '425px';
-    iframe.style.height = '85vh';
-    iframe.style.left = 'unset';
-    iframe.style.right = '1rem';
-    iframe.style.bottom = '8rem';
-    xMark.style.top = '10px';
-    xMark.style.right = '22px';
+  // Inject the CSS
+  const style = document.createElement('style');
+  style.innerHTML = `
+  .hidden {
+    display: none;
   }
-}
-updateIframeWidth();
-window.addEventListener('resize', updateIframeWidth);
+  #chat-widget-container {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    flex-direction: column;
+  }
+  #chat-popup {
+    height: 70vh;
+    max-height: 70vh;
+    transition: all 0.3s;
+    overflow: hidden;
+  }
+  @media (max-width: 768px) {
+    #chat-popup {
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      max-height: 100%;
+      border-radius: 0;
+      z-index: 2;
+    }
+  }
+  `;
 
-const chatDiv = document.createElement('div');
-chatDiv.setAttribute('id', 'chat-id');
-chatDiv.appendChild(container);
-document.body.appendChild(chatDiv);
+  document.head.appendChild(style);
+
+  // Create chat widget container
+  const chatWidgetContainer = document.createElement('div');
+  chatWidgetContainer.id = 'chat-widget-container';
+  document.body.appendChild(chatWidgetContainer);
+
+  const currentScript = document.currentScript;
+  const chatbotId = currentScript.getAttribute('data-chatbot-id');
+  const src = `https://leadqualifier.koretex.ai/?chatbotId=${chatbotId}`;
+
+  // Inject the HTML
+  chatWidgetContainer.innerHTML = `
+    <div id="chat-bubble" class="w-16 h-16  cursor-pointer text-3xl">
+      <img src="https://dashboard.koretex.ai/bubble.svg"/>
+    </div>
+    <div id="chat-popup" class="hidden absolute bottom-20 right-0 w-96 bg-white rounded-md shadow-md flex flex-col transition-all text-sm z-10">
+        <button id="close-popup" class="bg-transparent border-none text-white cursor-pointer absolute right-3 top-3 z-50">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+        <iframe id="chat-iframe"  src=${src} class="flex-grow z-10"></iframe>
+    </div>
+  `;
+
+  // Add event listeners
+  const chatBubble = document.getElementById('chat-bubble');
+  const closePopup = document.getElementById('close-popup');
+
+  chatBubble.addEventListener('click', function () {
+    togglePopup();
+  });
+
+  closePopup.addEventListener('click', function () {
+    togglePopup();
+  });
+
+  function togglePopup() {
+    const chatPopup = document.getElementById('chat-popup');
+    chatPopup.classList.toggle('hidden');
+  }
+})();
