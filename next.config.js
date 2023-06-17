8271e38ecb8c1120d1d3b15fb1c796b62a49e1a2
Pranav <pranavkp.me@outlook.com>
2023-06-17 16:21:54 +0530

    removed the minify

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
-
+  swcMinify: false,
   webpack(config, { isServer, dev }) {
     config.experiments = {
       asyncWebAssembly: true,
