747a9b356e0b4504f085725dcfe2806aaa0a2887
Pranav <pranavkp.me@outlook.com>
2023-06-20 16:16:45 +0530

    consoling values

--- public/chat-bot-bubble.js
+++ public/chat-bot-bubble.js
@@ -42,14 +42,19 @@ window.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY;
   const windowHeight = window.innerHeight;
   const halfScreen = windowHeight / 2;
+  console.log('windowHeight', windowHeight);
   console.log('half screen touch', halfScreen);
+  console.log('scrollPosition', scrollPosition);
   const chatbotContainer = document.getElementById('my-iframeContainer');
+  const closeButton = document.getElementById('closeButton');
 
   if (scrollPosition > halfScreen) {
     console.log('showing');
     chatbotContainer.style.display = 'block';
+    closeButton.style.display = 'block';
   } else {
     chatbotContainer.style.display = 'none';
+    closeButton.style.display = 'none';
   }
 });
 
@@ -73,6 +78,7 @@ xMark.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" f
   <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
 </svg>
 `;
+xMark.id = 'closeButton';
 xMark.style.position = 'absolute';
 xMark.style.width = '28px';
 xMark.style.height = '28x';
